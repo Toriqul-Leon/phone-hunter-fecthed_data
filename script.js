0aa674a31141ab9a678588e5a53f73f9186dceb4
Toriqul-Leon <yeitoriqul@gmail.com>
2022-03-01 01:00:53 +0600

    Search and search by ID added

--- script.js
+++ script.js
@@ -8,8 +8,9 @@ fetchData();
 
 const loadData = (phones) => {
   const sectionContainer = document.getElementById("section-container");
+
   for (const phone of phones) {
-    console.log(phone);
+    // console.log(phone);
     const article = document.createElement("article");
     article.innerHTML = ` <article  class="phone-container">
 
@@ -24,10 +25,60 @@ const loadData = (phones) => {
     
     <div class="line-view"></div>
     <div class="button-placement">  
-      <button class="main-btn">Details</button>
+      <button onclick="loadSingleData('${phone.slug}')" class="main-btn">Details</button>
 </div>
-
  </article>`;
     sectionContainer.appendChild(article);
   }
+  sectionContainer.textContent = "";
+};
+
+// !Managing Search search-text
+document.getElementById("search-btn").addEventListener("click", () => {
+  const searchText = document.getElementById("search-text").value;
+  const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
+  fetch(url)
+    .then((res) => res.json())
+    .then((search) => searchResult(search.data));
+});
+
+// !Search Result
+const searchResult = (results) => {
+  const sectionContainer = document.getElementById("section-container");
+  sectionContainer.textContent = "";
+  //   console.log(results);
+  for (const data of results) {
+    // console.log(data.phone_name);
+    const article = document.createElement("article");
+    article.innerHTML = ` <article  class="phone-container">
+
+    <div>
+     <img class="main-image" src="${data.image}" alt="" width="150" height="200">
+   </div>
+       <div class="text-content"> 
+         <div >
+        <h3>${data.phone_name}</h3>
+      </div>
+      <p class="brand-name"> <i class="fa-solid fa-mobile-button"></i></i> ${data.brand}</p>
+    
+    <div class="line-view"></div>
+    <div class="button-placement">  
+      <button onclick="loadSingleData('${data.slug}')" class="main-btn">Details</button>
+</div>
+ </article>`;
+    sectionContainer.appendChild(article);
+  }
+};
+
+// !Details
+const loadSingleData = (phone_id) => {
+  const url = `https://openapi.programming-hero.com/api/phone/${phone_id}`;
+  console.log(url);
+  fetch(url)
+    .then((res) => res.json())
+    .then((singlePhone) => showDetails(singlePhone.data));
+};
+
+const showDetails = (details) => {
+  console.log(details);
 };
